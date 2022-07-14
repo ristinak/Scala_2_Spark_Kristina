e56d24b53a3ad3f2cc7ea86ba620de2908c16b87
ristinak <ristinak@gmail.com>
2022-07-14 11:24:09 +0100

    Day 14 Cassandra initial commit

---
+++ src/main/scala/CassandraExample.scala
@@ -0,0 +1,115 @@
+package com.github.ristinak
+
+
+import com.datastax.driver.core._
+
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
+import java.io.FileInputStream
+import java.security.KeyStore
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import scala.collection.mutable.ArrayBuffer
+
+object CassandraExample {
+
+  def getClusterSession(host: String, port: Int, username: String, password: String, caPath: String): (Cluster,Session) = {
+    println(s"Going to connect to $host")
+    val sslOptions = loadCaCert(caPath)
+    //    var cluster = null
+    val cluster = Cluster.builder.addContactPoint(host).withPort(port).withSSL(sslOptions).withAuthProvider(new PlainTextAuthProvider(username, password)).build
+    (cluster, cluster.connect) //not throwing exception
+  }
+
+  def runQuery(session: Session, keyspace: String, query: String):ResultSet = {
+    session.execute(s"USE $keyspace") //use this database //no validity is provided here
+    session.execute(query) //so for non SELECTS nothing will be returned
+  }
+
+  @throws[Exception]
+  def cassandraExample(host: String, port: Int, username: String, password: String, caPath: String): Unit = {
+    println(s"Going to connect to $host")
+    val sslOptions = loadCaCert(caPath)
+    //    var cluster = null
+    val cluster = Cluster.builder.addContactPoint(host).withPort(port).withSSL(sslOptions).withAuthProvider(new PlainTextAuthProvider(username, password)).build
+    try {
+      //      cluster: Cluster = Cluster.builder.addContactPoint(host).withPort(port).withSSL(sslOptions).withAuthProvider(new PlainTextAuthProvider(username, password)).build
+      val session = cluster.connect
+      //first we create a new keyspace if it does not exist //so similar to creating a database
+      session.execute("CREATE KEYSPACE IF NOT EXISTS example_keyspace WITH REPLICATION = {'class': 'NetworkTopologyStrategy', 'aiven': 3}")
+      session.execute("USE example_keyspace") //use this database
+      //now we create a new table if we do not have it
+      session.execute("CREATE TABLE IF NOT EXISTS example_java (id int PRIMARY KEY, message text)")
+      //similar to prepared statements in SQL we bind values
+      //so to protect against CQL injection we bind the values
+      session.execute("INSERT INTO example_java (id, message) VALUES (?, ?)", 123, "Hello from Java-Scala!")
+      session.execute("INSERT INTO example_java (id, message) VALUES (?, ?)", 90, "Hello from Valdis")
+      val rs = session.execute("SELECT id, message FROM example_java")
+      import scala.jdk.CollectionConverters._
+      //      val row = rs.one()
+      rs.forEach(row => println(String.format("Row: id = %d, message = %s", row.getInt("id"), row.getString("message"))))
+
+      //      while ( rs.next() ) {
+      //
+      //      }
+      //      for (row <- rs) {
+      //        System.out.println(String.format("Row: id = %d, message = %s", row.getInt("id"), row.getString("message")))
+      //      }
+    } finally if (cluster != null) cluster.close()
+  }
+
+  @throws[Exception]
+  def setResults(host: String, port: Int, username: String, password: String, caPath: String, keyspace:String, id:Int, message:String): Unit = {
+    println(s"Going to connect to $host")
+    val sslOptions = loadCaCert(caPath)
+    //    var cluster = null
+    val cluster = Cluster.builder.addContactPoint(host).withPort(port).withSSL(sslOptions).withAuthProvider(new PlainTextAuthProvider(username, password)).build
+    try {
+      //      cluster: Cluster = Cluster.builder.addContactPoint(host).withPort(port).withSSL(sslOptions).withAuthProvider(new PlainTextAuthProvider(username, password)).build
+      val session = cluster.connect
+      session.execute(s"USE $keyspace") //FIXME worry about CQL injection attacks, this might not be safe if you depend on outside CQL
+      //TODO remove hardcoded table example_java
+      val rs = session.execute("INSERT INTO example_java (id, message) VALUES (?, ?)", id, message) //TODO think accepting arbitrary string safety
+    }finally if (cluster != null) cluster.close()
+  }
+
+
+
+  @throws[Exception]
+  def getResults(host: String, port: Int, username: String, password: String, caPath: String, keyspace:String, query: String): Seq[String] = {
+    println(s"Going to connect to $host")
+    val arrayBuf = ArrayBuffer[String]()
+    val sslOptions = loadCaCert(caPath)
+    //    var cluster = null
+    val cluster = Cluster.builder.addContactPoint(host).withPort(port).withSSL(sslOptions).withAuthProvider(new PlainTextAuthProvider(username, password)).build
+    try {
+      //      cluster: Cluster = Cluster.builder.addContactPoint(host).withPort(port).withSSL(sslOptions).withAuthProvider(new PlainTextAuthProvider(username, password)).build
+      val session = cluster.connect
+      session.execute(s"USE $keyspace") //FIXME worry about CQL injection attacks
+      val rs = session.execute(query) //TODO think accepting arbitrary string safety
+      rs.forEach(row => arrayBuf += String.format("Row: id = %d, message = %s", row.getInt("id"), row.getString("message")))
+
+    }finally if (cluster != null) cluster.close() //closes session AND cluster (some extra options)
+    arrayBuf.toSeq
+  }
+
+  @throws[Exception]
+  private def loadCaCert(caCertPath: String) = {
+    val cf = CertificateFactory.getInstance("X.509")
+    var fis: FileInputStream = null
+    var caCert: X509Certificate = null
+    try {
+      fis = new FileInputStream(caCertPath)
+      caCert = cf.generateCertificate(fis).asInstanceOf[X509Certificate]
+    } finally if (fis != null) fis.close()
+    val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
+    val ks = KeyStore.getInstance(KeyStore.getDefaultType)
+    ks.load(null)
+    ks.setCertificateEntry("caCert", caCert)
+    tmf.init(ks)
+    val sslContext = SSLContext.getInstance("TLS")
+    sslContext.init(null, tmf.getTrustManagers, null)
+    RemoteEndpointAwareJdkSSLOptions.builder.withSSLContext(sslContext).build
+  }
+}
+
---
+++ src/main/scala/Day14CassandraClient.scala
@@ -0,0 +1,48 @@
+package com.github.ristinak
+
+import CassandraExample.{cassandraExample, getResults, getClusterSession, runQuery, setResults}
+
+import scala.collection.mutable.ArrayBuffer
+
+case class Message(id: Int, msg: String)
+
+object Day14CassandraClient extends App {
+  println("Testing Cassandra")
+
+  val host = "cassandra-1255d130-ristinak-37bf.aivencloud.com" //your url will be slightly different
+  val port = 17638 //port might also be a different
+  val username = "avnadmin" //most likely the same
+  val password = scala.util.Properties.envOrElse("CASSANDRA_PW", "na")
+  //  val caPath = "C:\\certs\\"
+  val caPath = "./src/resources/certs/ca.pem" //you need to download your own cert, do not commit to GIT!!
+
+  cassandraExample(host=host,port=port,username=username,password=password, caPath=caPath)
+  println("Lets hold our fingers crossed")
+  setResults(host=host,
+    port=port,
+    username=username,
+    password=password,
+    caPath=caPath,
+    keyspace = "example_keyspace",
+    id = 9000,
+    message = "Hello from Space on Jul 12nd")
+
+  val query = "SELECT id, message FROM example_java"
+  val results = getResults(host=host,port=port,username=username,password=password, caPath=caPath, keyspace = "example_keyspace", query = query)
+  results.foreach(println)
+
+  //we unpack the tuple from getClusterSession,because we do not want to use mytuple._1 and mytuple._2
+  val (cluster, session) = getClusterSession(host=host,port=port,username=username,password=password, caPath=caPath)
+  val rSet = runQuery(session, "example_keyspace", "SELECT id, message FROM example_java" )
+  val resultBuffer = ArrayBuffer[Message]()
+  rSet.forEach(row => resultBuffer += Message(row.getInt("id"), row.getString("message")))
+  val resultArray = resultBuffer.toArray
+  println(resultArray.mkString(","))
+  //  runQuery(session, "example_keyspace", "ALTER TABLE example_java ADD created timestamp")
+  val newResultSet = runQuery(session, "example_keyspace", "SELECT * FROM example_java" )
+  newResultSet.forEach(row => println(s"$row"))
+  //  rSet.forEach(row => println(s"ID: ${row.getInt("id")}, message: ${row.getString("message")}"))
+  cluster.close() //we cleanup the connection, otherwise our program continues to run
+}
+
+
